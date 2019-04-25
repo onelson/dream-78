8030702988bb7cb80dac82596930ae7fe86e6ab5
Owen Nelson <onelson@gmail.com>
2019-04-25 13:29:56 -0700

    reworked version of gltf example program

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 extern crate amethyst;
-#[macro_use]
-extern crate log;
-
 use amethyst::{
+    animation::{
+        get_animation_set, AnimationBundle, AnimationCommand, AnimationControlSet, AnimationSet,
+        EndControl, VertexSkinningBundle,
+    },
+    assets::{
+        AssetPrefab, Completion, Handle, Prefab, PrefabData, PrefabError, PrefabLoader,
+        PrefabLoaderSystem, ProgressCounter, RonFormat,
+    },
+    controls::{ControlTagPrefab, FlyControlBundle},
+    core::transform::{Transform, TransformBundle},
+    ecs::prelude::{Entity, ReadStorage, Write, WriteStorage},
+    input::{is_close_requested, is_key_down},
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
-    utils::application_root_dir,
+    renderer::{CameraPrefab, DrawPbmSeparate, LightPrefab, VirtualKeyCode},
+    utils::{
+        application_root_dir,
+        tag::{Tag, TagFinder},
+    },
 };
+use amethyst_derive::PrefabData;
+use amethyst_gltf::{GltfSceneAsset, GltfSceneFormat, GltfSceneLoaderSystem};
+use serde::{Deserialize, Serialize};
 
 use std::env;
 use std::fs;
@@ -23,37 +38,190 @@ fn get_gltf_file() -> io::Result<PathBuf> {
     }
 }
 
-struct Example;
+#[derive(Clone, Serialize, Deserialize)]
+struct AnimationMarker;
 
-impl SimpleState for Example {}
+#[derive(Default, Deserialize, Serialize, PrefabData)]
+#[serde(default)]
+struct ScenePrefabData {
+    transform: Option<Transform>,
+    gltf: Option<AssetPrefab<GltfSceneAsset, GltfSceneFormat>>,
+    camera: Option<CameraPrefab>,
+    light: Option<LightPrefab>,
+    tag: Option<Tag<AnimationMarker>>,
+    fly_tag: Option<ControlTagPrefab>,
+}
+
+#[derive(Default)]
+struct Scene {
+    handle: Option<Handle<Prefab<ScenePrefabData>>>,
+    animation_index: usize,
+}
+
+#[derive(Default)]
+struct Example {
+    entity: Option<Entity>,
+    initialised: bool,
+    progress: Option<ProgressCounter>,
+}
+
+impl SimpleState for Example {
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        let StateData { world, .. } = data;
+
+        self.progress = Some(ProgressCounter::default());
+
+        world.exec(
+            |(loader, mut scene): (PrefabLoader<'_, ScenePrefabData>, Write<'_, Scene>)| {
+                scene.handle = Some(loader.load(
+                    "suzanne_scene.ron",
+                    RonFormat,
+                    (),
+                    self.progress.as_mut().unwrap(),
+                ));
+            },
+        );
+    }
+
+    fn handle_event(
+        &mut self,
+        data: StateData<'_, GameData<'_, '_>>,
+        event: StateEvent,
+    ) -> SimpleTrans {
+        let StateData { world, .. } = data;
+        if let StateEvent::Window(event) = &event {
+            if is_close_requested(&event) || is_key_down(&event, VirtualKeyCode::Escape) {
+                Trans::Quit
+            } else if is_key_down(&event, VirtualKeyCode::Space) {
+                toggle_or_cycle_animation(
+                    self.entity,
+                    &mut world.write_resource(),
+                    &world.read_storage(),
+                    &mut world.write_storage(),
+                );
+                Trans::None
+            } else {
+                Trans::None
+            }
+        } else {
+            Trans::None
+        }
+    }
+
+    fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        if !self.initialised {
+            let remove = match self.progress.as_ref().map(|p| p.complete()) {
+                None | Some(Completion::Loading) => false,
+
+                Some(Completion::Complete) => {
+                    let scene_handle = data
+                        .world
+                        .read_resource::<Scene>()
+                        .handle
+                        .as_ref()
+                        .unwrap()
+                        .clone();
+
+                    data.world.create_entity().with(scene_handle).build();
+
+                    true
+                }
+
+                Some(Completion::Failed) => {
+                    println!("Error: {:?}", self.progress.as_ref().unwrap().errors());
+                    return Trans::Quit;
+                }
+            };
+            if remove {
+                self.progress = None;
+            }
+            if self.entity.is_none() {
+                if let Some(entity) = data
+                    .world
+                    .exec(|finder: TagFinder<'_, AnimationMarker>| finder.find())
+                {
+                    self.entity = Some(entity);
+                    self.initialised = true;
+                }
+            }
+        }
+        Trans::None
+    }
+}
+
+fn toggle_or_cycle_animation(
+    entity: Option<Entity>,
+    scene: &mut Scene,
+    sets: &ReadStorage<'_, AnimationSet<usize, Transform>>,
+    controls: &mut WriteStorage<'_, AnimationControlSet<usize, Transform>>,
+) {
+    if let Some((entity, Some(animations))) = entity.map(|entity| (entity, sets.get(entity))) {
+        if animations.animations.len() > scene.animation_index {
+            let animation = animations.animations.get(&scene.animation_index).unwrap();
+            let set = get_animation_set::<usize, Transform>(controls, entity).unwrap();
+            if set.has_animation(scene.animation_index) {
+                set.toggle(scene.animation_index);
+            } else {
+                println!("Running animation {}", scene.animation_index);
+                set.add_animation(
+                    scene.animation_index,
+                    animation,
+                    EndControl::Normal,
+                    1.0,
+                    AnimationCommand::Start,
+                );
+            }
+            scene.animation_index += 1;
+            if scene.animation_index >= animations.animations.len() {
+                scene.animation_index = 0;
+            }
+        }
+    }
+}
 
 fn main() -> amethyst::Result<()> {
-    amethyst::Logger::from_config(amethyst::LoggerConfig {
-        level_filter: amethyst::LogLevelFilter::Info,
-        ..Default::default()
-    })
-    .level_for("gfx_device_gl", amethyst::LogLevelFilter::Warn)
-    .level_for("dream78", amethyst::LogLevelFilter::Debug)
-    .start();
+    amethyst::Logger::from_config(Default::default())
+        //    .level_for("gfx_device_gl", amethyst::LogLevelFilter::Warn)
+        .level_for("dream78", amethyst::LogLevelFilter::Debug)
+        .start();
 
-    let fp = get_gltf_file().expect("glTF file missing or invalid");
+    let app_root: PathBuf = application_root_dir().into();
+    let resources_directory = app_root.join("assets/");
+    let display_conf = app_root.join("resources/display_config.ron");
 
-    debug!("Loading `{}`", fp.display());
+    let game_data = GameDataBuilder::default()
+        .with(
+            PrefabLoaderSystem::<ScenePrefabData>::default(),
+            "scene_loader",
+            &[],
+        )
+        .with(
+            GltfSceneLoaderSystem::default(),
+            "gltf_loader",
+            &["scene_loader"], // This is important so that entity instantiation is performed in a single frame.
+        )
+        .with_basic_renderer(display_conf, DrawPbmSeparate::new(), false)?
+        .with_bundle(
+            AnimationBundle::<usize, Transform>::new("animation_control", "sampler_interpolation")
+                .with_dep(&["gltf_loader"]),
+        )?
+        .with_bundle(
+            FlyControlBundle::<String, String>::new(None, None, None)
+                .with_sensitivity(0.1, 0.1)
+                .with_speed(5.),
+        )?
+        .with_bundle(TransformBundle::new().with_dep(&[
+            "animation_control",
+            "sampler_interpolation",
+            "fly_movement",
+        ]))?
+        .with_bundle(VertexSkinningBundle::new().with_dep(&[
+            "transform_system",
+            "animation_control",
+            "sampler_interpolation",
+        ]))?;
 
-    let path = format!("{}/resources/display_config.ron", application_root_dir());
-    let config = DisplayConfig::load(&path);
-
-    let pipe = Pipeline::build().with_stage(
-        Stage::with_backbuffer()
-            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
-    );
-
-    let game_data =
-        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
-    let mut game = Application::new("./", Example, game_data)?;
-
+    let mut game = Application::build(resources_directory, Example::default())?.build(game_data)?;
     game.run();
-
     Ok(())
 }
